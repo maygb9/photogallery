82b060c85a0d86fa56e2d406d2ab06ed05962c41
maygb9 <44600000+maygb9@users.noreply.github.com>
2019-05-05 00:59:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-
-// Comment these lines to hide errors
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require 'includes/config.php';
-require 'includes/functions.php';
-
-init();
+<?php include_once("home.html"); ?>
